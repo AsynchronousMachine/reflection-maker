3e8e4e0a0710f15afe36767755ffbdaf17b680bc
Egmond Krebs <e.krebs@erhardt-leimer.com>
2016-11-23 12:51:22 +0100

    write maps to file

--- map_maker.cpp
+++ map_maker.cpp
@@ -7,14 +7,14 @@
 #include <vector>
 #include <unordered_map>
 #include <string>
-#include <iostream>
+#include <fstream>
 
 // see AST matcher reference for available macros at http://clang.llvm.org/docs/LibASTMatchersReference.html
 using namespace clang::ast_matchers;
 
-auto DOFieldMatcher = fieldDecl(hasType(recordDecl(hasName("DataObject")))).bind("DO");
-auto LinkFieldMatcher = fieldDecl(hasType(recordDecl(hasName("Link")))).bind("link");
-auto ModuleClassMatcher = cxxRecordDecl(hasMethod(hasName("deserialize")), hasMethod(hasName("serialize")), hasMethod(hasName("getName")), has(DOFieldMatcher)).bind("moduleClass");
+auto DOFieldMatcher = fieldDecl(isPublic(), hasType(recordDecl(hasName("DataObject")))).bind("DO");
+auto LinkFieldMatcher = fieldDecl(isPublic(), hasType(recordDecl(hasName("Link")))).bind("link");
+auto ModuleClassMatcher = cxxRecordDecl(hasMethod(hasName("deserialize")), hasMethod(hasName("serialize")), anyOf(has(DOFieldMatcher), has(LinkFieldMatcher))).bind("moduleClass");
 auto ModuleInstanceMatcher = varDecl(hasType(ModuleClassMatcher)).bind("moduleInstance");
 
 std::unordered_multimap<std::string, std::string> DOs; // key: class, value: DO
@@ -24,84 +24,152 @@ std::unordered_map<std::string, std::string> ModuleInstances; // key: variable, 
 class MapMaker : public clang::ast_matchers::MatchFinder::MatchCallback
 {
 public:
-    void run(const clang::ast_matchers::MatchFinder::MatchResult &Result) override
-    {
-        if (const clang::RecordDecl *rd = Result.Nodes.getNodeAs<clang::RecordDecl>("moduleClass"))
-        {
-            std::string moduleClass = rd->getNameAsString();
-            if (const clang::VarDecl *vd = Result.Nodes.getNodeAs<clang::VarDecl>("moduleInstance"))
-                ModuleInstances.emplace(vd->getNameAsString(), moduleClass);
-        }
-        else if (const clang::FieldDecl *fd = Result.Nodes.getNodeAs<clang::FieldDecl>("DO"))
-            DOs.emplace(fd->getParent()->getNameAsString(), fd->getNameAsString());
-        else if (const clang::FieldDecl *fd = Result.Nodes.getNodeAs<clang::FieldDecl>("link"))
-            Links.emplace(fd->getParent()->getNameAsString(), fd->getNameAsString());
-    }
+	void run(const clang::ast_matchers::MatchFinder::MatchResult &Result) override
+	{
+		if (const clang::RecordDecl *rd = Result.Nodes.getNodeAs<clang::RecordDecl>("moduleClass"))
+		{
+			std::string moduleClass = rd->getNameAsString();
+			if (const clang::VarDecl *vd = Result.Nodes.getNodeAs<clang::VarDecl>("moduleInstance")) {
+				ModuleInstances.emplace(vd->getNameAsString(), moduleClass);
+			}
+		}
+		else if (const clang::FieldDecl *fd = Result.Nodes.getNodeAs<clang::FieldDecl>("DO")) {
+			DOs.emplace(fd->getParent()->getNameAsString(), fd->getNameAsString());
+		}
+		else if (const clang::FieldDecl *fd = Result.Nodes.getNodeAs<clang::FieldDecl>("link")) {
+			Links.emplace(fd->getParent()->getNameAsString(), fd->getNameAsString());
+		}
+	}
 };
 
 static llvm::cl::OptionCategory MapMakerCategory("MapMaker options");
-static llvm::cl::extrahelp CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
+//static llvm::cl::extrahelp CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
 
 int main(int argc, const char **argv)
 {
-    clang::tooling::CommonOptionsParser OptionsParser(argc, argv, MapMakerCategory);
-    clang::tooling::ClangTool Tool(OptionsParser.getCompilations(), OptionsParser.getSourcePathList());
+	clang::tooling::CommonOptionsParser OptionsParser(argc, argv, MapMakerCategory);
+	clang::tooling::ClangTool Tool(OptionsParser.getCompilations(), OptionsParser.getSourcePathList());
 
-    MapMaker Maker;
-    clang::ast_matchers::MatchFinder Finder;
-    Finder.addMatcher(DOFieldMatcher, &Maker);
-    Finder.addMatcher(LinkFieldMatcher, &Maker);
-    Finder.addMatcher(ModuleInstanceMatcher, &Maker);
+	MapMaker Maker;
+	clang::ast_matchers::MatchFinder Finder;
+	Finder.addMatcher(DOFieldMatcher, &Maker);
+	Finder.addMatcher(LinkFieldMatcher, &Maker);
+	Finder.addMatcher(ModuleInstanceMatcher, &Maker);
 
-    int Result = Tool.run(clang::tooling::newFrontendActionFactory(&Finder).get());
+	int Result = Tool.run(clang::tooling::newFrontendActionFactory(&Finder).get());
 
-    std::cout << "using dataobject_map = std::unordered_map<std::string, boost::any>;\n";
-    std::cout << "dataobject_map dos{\n";
-    for (auto &ModuleInstance : ModuleInstances)
-    {
-        auto range = DOs.equal_range(ModuleInstance.second);
-        for (auto it = range.first; it != range.second; ++it)
-        {
-            std::cout << "{" + ModuleInstance.first + ".getName() + \".\" + " + ModuleInstance.first + "." + it->second + ".getName(), &" + ModuleInstance.first + "." + it->second + "},\n";
-        }
-    }
-    std::cout << "};\n";
+	std::fstream fs;
 
-    std::cout << "using registerlink_map = std::unordered_map<std::string, std::function<void(std::string, boost::any, boost::any)>>;\n";
-    std::cout << "registerlink_map set_links{\n";
-    for (auto &ModuleInstance : ModuleInstances)
-    {
-        auto range = Links.equal_range(ModuleInstance.second);
-        for (auto it = range.first; it != range.second; ++it)
-        {
-            std::cout << "{" + ModuleInstance.first + ".getName() + \".\" + " + ModuleInstance.first + "." + it->second + ".getName(), [&" + ModuleInstance.first + "](std::string name, boost::any a1, boost::any a2){auto l = std::mem_fn(&" + ModuleInstance.second + "::" + it->second + "); l(" + ModuleInstance.first + ").set(name, a1, a2);}},\n";
-        }
-    }
-    std::cout << "};\n";
+	fs.open("global_reflection.h", std::fstream::out);
 
-    std::cout << "using unregisterlink_map = std::unordered_map<std::string, std::function<void(std::string, boost::any)>>;\n";
-    std::cout << "unregisterlink_map clear_links{\n";
-    for (auto &ModuleInstance : ModuleInstances)
-    {
-        auto range = Links.equal_range(ModuleInstance.second);
-        for (auto it = range.first; it != range.second; ++it)
-        {
-            std::cout << "{" + ModuleInstance.first + ".getName() + \".\" + " + ModuleInstance.first + "." + it->second + ".getName(), [&" + ModuleInstance.first + "](std::string name, boost::any a){auto l = std::mem_fn(&" + ModuleInstance.second + "::" + it->second + "); l(" + ModuleInstance.first + ").clear(name, a);}},\n";
-        }
-    }
-    std::cout << "};\n";
+	fs << "#pragma once" << std::endl
+		<< "#include <unordered_map>" << std::endl
+		<< "#include <functional>" << std::endl
+		<< std::endl
+		<< "#include <boost/any.hpp>" << std::endl
+		<< std::endl
+		<< "using module_name_map = std::unordered_map<void*, std::string>;" << std::endl
+		<< "using name_module_map = std::unordered_map<std::string, boost::any>;" << std::endl
+		<< "using dataobject_name_map = std::unordered_map<void*, std::string>;" << std::endl
+		<< "using name_dataobject_map = std::unordered_map<std::string, boost::any>;" << std::endl
+		<< "using doset_map = std::unordered_map<std::string, std::function<void(const boost::any)>>;" << std::endl
+		<< "using registerlink_map = std::unordered_map<std::string, std::function<void(std::string, boost::any, boost::any)>>;" << std::endl
+		<< "using unregisterlink_map = std::unordered_map<std::string, std::function<void(std::string, boost::any)>>;" << std::endl
+		<< std::endl
+		<< "extern const module_name_map module_names;" << std::endl
+		<< "extern const name_module_map modules;" << std::endl
+		<< "extern const dataobject_name_map do_names;" << std::endl
+		<< "extern const name_dataobject_map dos;" << std::endl
+		<< "extern const doset_map do_set;" << std::endl
+		<< "extern const registerlink_map set_links;" << std::endl
+		<< "extern const unregisterlink_map clear_links;" << std::endl;
 
-    std::cout << "using deserialize_map = std::unordered_map<std::string, std::function<void(std::string)>>;\n";
-    std::cout << "const deserialize_map des_modules{\n";
-    for (auto &ModuleInstance : ModuleInstances)
-        std::cout << "{" + ModuleInstance.first + ".getName(), [&" + ModuleInstance.first + "](std::string js){std::mem_fn(&" + ModuleInstance.second + "::deserialize)(" + ModuleInstance.first + ", js);}},\n";
-    std::cout << "};\n";
+	fs.close();
 
-    std::cout << "using serialize_map = std::unordered_map<std::string, std::function<std::string()>>;\n";
-    std::cout << "const serialize_map s_modules{\n";
-    for (auto &ModuleInstance : ModuleInstances)
-        std::cout << "{" + ModuleInstance.first + ".getName(), [&" + ModuleInstance.first + "](){return std::mem_fn(&" + ModuleInstance.second + "::serialize)(" + ModuleInstance.first + ");}},\n";
-    std::cout << "};\n";
 
-    return Result;
+
+	fs.open("global_reflection.cpp", std::fstream::out);
+
+	fs << "#include \"global_reflection.h\"" << std::endl << std::endl;
+	fs << "const name_module_map modules" << std::endl << "{" << std::endl;
+	for (auto &ModuleInstance : ModuleInstances)
+	{
+		fs << "\t{\"" + ModuleInstance.second + "." + ModuleInstance.first + "\", &" + ModuleInstance.first + "}";
+		fs << ",";
+		fs << std::endl;
+	}
+	fs << "};" << std::endl << std::endl;
+
+	fs << "// Get out the module name for humans" << std::endl;
+	fs << "const module_name_map module_names" << std::endl << "{" << std::endl;
+	for (auto &ModuleInstance : ModuleInstances)
+	{
+		fs << "\t{&" + ModuleInstance.first + ", \"" + ModuleInstance.second + "." + ModuleInstance.first + "\"}";
+		fs << ",";
+		fs << std::endl;
+	}
+	fs << "};" << std::endl << std::endl;
+
+	fs << "const name_dataobject_map dos" << std::endl << "{" << std::endl;
+	for (auto &ModuleInstance : ModuleInstances)
+	{
+		auto range = DOs.equal_range(ModuleInstance.second);
+		for (auto it = range.first; it != range.second; ++it)
+		{
+			fs << "\t{\"" + ModuleInstance.second + "." + ModuleInstance.first + "." + it->second + "\", &" + ModuleInstance.first + "." + it->second + "}," << std::endl;
+		}
+	}
+	fs << "};" << std::endl << std::endl;
+
+	fs << "// Get out the DO name for humans" << std::endl;
+	fs << "const dataobject_name_map do_names" << std::endl << "{" << std::endl;
+	for (auto &ModuleInstance : ModuleInstances)
+	{
+		auto range = DOs.equal_range(ModuleInstance.second);
+		for (auto it = range.first; it != range.second; ++it)
+		{
+			fs << "\t{&" + ModuleInstance.first + "." + it->second + ", \"" + ModuleInstance.second + "." + ModuleInstance.first + "." + it->second + "\"}," << std::endl;
+		}
+	}
+	fs << "};" << std::endl << std::endl;
+
+	fs << "const doset_map do_set" << std::endl << "{" << std::endl;;
+
+	for (auto &ModuleInstance : ModuleInstances)
+	{
+		auto range = DOs.equal_range(ModuleInstance.second);
+		for (auto it = range.first; it != range.second; ++it)
+		{
+			fs << "\t{\"" + ModuleInstance.second + "." + ModuleInstance.first + "." + it->second + "\", [](const boost::any &a) {" + ModuleInstance.first + "." + it->second + ".set(a); } }," << std::endl;
+		}
+	}
+	fs << "};" << std::endl << std::endl;
+
+	fs << "const registerlink_map set_links" << std::endl << "{" << std::endl;
+	for (auto &ModuleInstance : ModuleInstances)
+	{
+		auto range = Links.equal_range(ModuleInstance.second);
+		for (auto it = range.first; it != range.second; ++it)
+		{
+			fs << "\t{\"" + ModuleInstance.second + "." + ModuleInstance.first + "." + it->second + "\", [](std::string name, boost::any a1, boost::any a2) {" + ModuleInstance.first + "." + it->second + ".set(name, a1, a2); } }," << std::endl;
+		}
+	}
+	fs << "};" << std::endl << std::endl;
+
+
+	fs << "const unregisterlink_map clear_links" << std::endl << "{" << std::endl;;
+	for (auto &ModuleInstance : ModuleInstances)
+	{
+		auto range = Links.equal_range(ModuleInstance.second);
+		for (auto it = range.first; it != range.second; ++it)
+		{
+			fs << "\t{\"" + ModuleInstance.second + "." + ModuleInstance.first + "." + it->second + "\", [](std::string name, boost::any a) {" + ModuleInstance.first + "." + it->second + ".clear(name, a); } }," << std::endl;
+		}
+	}
+	fs << "};" << std::endl << std::endl;
+
+	fs.close();
+
+
+	return Result;
 }
